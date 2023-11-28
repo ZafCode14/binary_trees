bf10c2a4d6bd9579d9546b5fa9d9d1644352b02c
ZafCode14 <zafarwp14@gmail.com>
2023-11-28 12:52:04 +0200

    bst

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * is_bst_util - helper function to check if a binary tree is a valid BST
+ * @tree: Pointer to the root node of the tree
+ * @prev: Pointer to the previously visited node's value
+ *
+ * Return: 1 if the tree is a valid BST, 0 otherwise
+ */
+int is_bst_util(const binary_tree_t *tree, int *prev)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (!is_bst_util(tree->left, prev))
+		return (0);
+
+	if (tree->n <= *prev)
+		return (0);
+
+	*prev = tree->n;
+
+	return (is_bst_util(tree->right, prev));
+}
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid BST
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: 1 if the tree is a valid BST, 0 otherwise or if tree is NULL
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	int prev = INT_MIN;
+
+	if (tree == NULL)
+		return (0);
+
+	return (is_bst_util(tree, &prev));
+}
---
+++ 111-bst_insert.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - inserts a value into a Binary Search Tree (BST)
+ * @tree: Double pointer to the root node of the BST to insert the value
+ * @value: Value to store in the node to be inserted
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		if (*tree == NULL)
+			return (NULL);
+		return (*tree);
+	}
+
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			if ((*tree)->left == NULL)
+				return (NULL);
+			return ((*tree)->left);
+		}
+		else
+			return (bst_insert(&((*tree)->left), value));
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			if ((*tree)->right == NULL)
+				return (NULL);
+			return ((*tree)->right);
+		}
+		else
+			return (bst_insert(&((*tree)->right), value));
+	}
+
+	return (NULL);
+}
