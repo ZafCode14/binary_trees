d8b2e67df6ac6f97b6b488a386783c0604a859df
ZafCode14 <zafarwp14@gmail.com>
2023-11-28 13:10:14 +0200

    114

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,19 @@
 #include "binary_trees.h"
 
 /**
+ * bst_find_min - finds the minimum value node in a BST
+ * @root: Pointer to the root node of the BST
+ *
+ * Return: Pointer to the node containing the minimum value
+ */
+bst_t *bst_find_min(bst_t *root)
+{
+	while (root->left != NULL)
+		root = root->left;
+	return (root);
+}
+
+/**
  * bst_remove - removes a node from a Binary Search Tree (BST)
  * @root: Pointer to the root node of the tree
  * @value: Value to remove from the tree
@@ -32,11 +45,8 @@ bst_t *bst_remove(bst_t *root, int value)
 			free(root);
 			return (temp);
 		}
-		temp = root->right;
 
-		while (temp->left != NULL)
-			temp = temp->left;
-
+		temp = bst_find_min(root->right);
 		root->n = temp->n;
 		root->right = bst_remove(root->right, temp->n);
 	}
