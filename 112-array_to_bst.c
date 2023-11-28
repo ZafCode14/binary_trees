c99d1a3166de692e40a03d0ffaab6c78805f59b5
ZafCode14 <zafarwp14@gmail.com>
2023-11-28 12:59:04 +0200

    112

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ * bst_free - frees a Binary Search Tree (BST)
+ * @tree: Pointer to the root node of the BST
+ *
+ * Return: void
+ */
+void bst_free(bst_t *tree)
+{
+	if (tree != NULL)
+	{
+		bst_free(tree->left);
+		bst_free(tree->right);
+		free(tree);
+	}
+}
+
+/**
  * array_to_bst - builds a Binary Search Tree (BST) from an array
  * @array: Pointer to the first element of the array to be converted
  * @size: Number of elements in the array
@@ -16,8 +32,13 @@ bst_t *array_to_bst(int *array, size_t size)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
+	{
 		if (bst_insert(&root, array[i]) == NULL)
+		{
+			bst_free(root);
 			return (NULL);
+		}
+	}
 
 	return (root);
 }
